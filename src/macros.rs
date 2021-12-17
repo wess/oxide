ddd309a7e3be1f0ba7d072ab209a772cb988910c
wess <wcope@me.com>
2021-12-17 13:45:02 -0500

    Updated macro for either

--- src/macros.rs
+++ src/macros.rs
@@ -9,9 +9,32 @@
 //
 
 #[macro_export]
-macro_rules! string {
-  ($str:expr) => (String::from($str);)
+macro_rules! either {
+  ($test:expr => $truthy:expr , $falsy:expr) => {
+    if $test {
+      $truthy
+    } else {
+      $falsy
+    }
+  }
 }
+
+#[macro_export]
+macro_rules! string {
+  ($str:expr) => {{ String::from($str) }}
+}
+
+#[macro_export]
+macro_rules! string_combined {
+  ($head:expr,$tail:expr) => {{
+    let mut destination = String::from($head);
+    let appending = String::from($tail);
+
+    destination.push_str(&appending);
+
+    String::from(destination)
+  }}
+ }
 
 #[macro_export]
 macro_rules! map {
@@ -34,6 +57,52 @@ macro_rules! list {
 }
 
 #[macro_export]
-macro_rules! armu {
+macro_rules! arc_mu {
   ($var:expr) => (Arc::new(Mutex::new($var));)
+}
+
+#[macro_export]
+macro_rules! file_write {
+  ($path:expr, $data:expr) => {{
+    let mut file = match File::create($path) {
+      Ok(file) => file,
+      Err(e) => {
+        console_panic!("Unable to create file: {}", e);
+      }
+    };
+
+    file.write_all($data.as_bytes())
+  }};
+}
+
+#[macro_export]
+macro_rules! file_read {
+  ($path:expr) => {{
+    let mut file = match std::fs::File::open($path) {
+      Ok(file) => file,
+      Err(e) => {
+        console_panic!("Unable to open file: {}", e);
+      }
+    };
+
+    let mut buffer = String::new();
+
+    match file.read_to_string(&mut buffer) {
+      Ok(_) => {},
+      Err(e) => {
+        console_panic!("Unable to read file: {}", e);
+      }
+    };
+
+    buffer.clone()
+  }};
+}
+
+#[macro_export]
+macro_rules! file_exists {
+  ($path:expr) => {{
+    let path = Path::new($path);
+
+    path.exists()
+  }};
 }
